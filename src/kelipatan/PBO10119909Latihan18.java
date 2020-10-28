39d35d11e507aa55ea4efea5db29dda50783267f
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-28 13:03:06 +0700

    Add files via upload

---
+++ src/kelipatan/PBO10119909Latihan18.java
@@ -0,0 +1,19 @@
+package kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+
+
+public class PBO10119909Latihan18 {
+
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println("i : "+i);
+        }
+    }
+}
