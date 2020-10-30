222a521c654554ef4b3ca23d2435125f2a5e6584
Anggawan Ridho <anggawanridho@gmail.com>
2020-10-30 22:56:58 +0700

    revise "i"

--- src/kelipatan/PBO10119909Latihan18.java
+++ src/kelipatan/PBO10119909Latihan18.java
@@ -13,7 +13,7 @@ public class PBO10119909Latihan18 {
 
     public static void main(String[] args) {
         for (double i = 3.5; i <= 35; i += 3.5) {
-            System.out.println("i : "+i);
+            System.out.println(i);
         }
     }
 }
